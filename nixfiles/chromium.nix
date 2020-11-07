3a8e4a2678a8599e4202bc8967d267d026fcfdba
Jesse Del Greco <jesse@jessedelgreco.com>
2020-11-07 13:30:52 -0500

    flatten reference bookmarks

--- nixfiles/chromium.nix
+++ nixfiles/chromium.nix
@@ -137,24 +137,14 @@
           { name = "newegg"; url = "https://newegg.com/"; }
           { name = "twitter"; url = "https://twitter.com/"; }
         ]; }
-        { name = "Nix"; children = [
-          { name = "nix-manual"; url = "https://nixos.org/nix/manual/"; }
-          { name = "nixos-discourse"; url = "https://discourse.nixos.org/"; }
-          { name = "nixos-manual"; url = "https://nixos.org/nixos/manual/"; }
-          { name = "nixos-options"; url = "https://nixos.org/nixos/options.html"; }
-          { name = "nixos-reddit"; url = "https://old.reddit.com/r/nixos/"; }
-          { name = "nixos-wiki"; url = "https://nixos.wiki/"; }
-          { name = "nixpkgs-manual"; url = "https://nixos.org/nixpkgs/manual/"; }
-        ]; }
-        { name = "ZFS"; children = [
-          { name = "zfs-archwiki"; url = "https://wiki.archlinux.org/index.php/ZFS"; }
-          { name = "zfs-gentoowiki"; url = "https://wiki.gentoo.org/wiki/ZFS"; }
-          { name = "zfs-handbook"; url = "https://www.freebsd.org/doc/handbook/zfs.html"; }
-          { name = "zfs-nixoswiki"; url = "https://nixos.wiki/wiki/NixOS_on_ZFS"; }
-          { name = "zfs-oracle"; url = "https://docs.oracle.com/cd/E26505_01/html/E37384/"; }
-          { name = "zfs-reddit"; url = "https://old.reddit.com/r/zfs/"; }
-        ]; }
-        { name = "Elixir"; children = [
+        { name = "Reference"; children = [
+          { name = "carbon"; url = "https://www.carbondesignsystem.com/"; }
+          { name = "carbon-icons"; url = "https://www.carbondesignsystem.com/guidelines/icons/library"; }
+          { name = "crystal-book"; url = "https://crystal-lang.org/reference/getting_started/"; }
+          { name = "crystal-docs"; url = "https://crystal-lang.org/docs/"; }
+          { name = "crystal-forum"; url = "https://forum.crystal-lang.org/"; }
+          { name = "crystal-lang"; url = "https://crystal-lang.org/"; }
+          { name = "crystal-playground"; url = "https://play.crystal-lang.org/#/cr"; }
           { name = "elixir-awesome"; url = "https://github.com/h4cc/awesome-elixir"; }
           { name = "elixir-blog"; url = "https://elixir-lang.org/blog"; }
           { name = "elixir-book"; url = "https://elixir-lang.org/getting-started/introduction.html"; }
@@ -178,34 +168,18 @@
           { name = "elixir-lang"; url = "https://elixir-lang.org/"; }
           { name = "elixir-reddit"; url = "https://old.reddit.com/r/elixir/"; }
           { name = "elixir-school"; url = "https://elixirschool.com/en/"; }
-        ]; }
-        { name = "Zig"; children = [
-          { name = "zig-awesome"; url = "https://github.com/nrdmn/awesome-zig"; }
-          { name = "zig-docs"; url = "https://ziglang.org/documentation/0.6.0/"; }
-          { name = "zig-docs-std"; url = "https://ziglang.org/documentation/0.6.0/std/"; }
-          { name = "zig-github"; url = "https://github.com/ziglang/zig"; }
-          { name = "zig-issues"; url = "https://github.com/ziglang/zig/issues"; }
-          { name = "zig-lang"; url = "https://ziglang.org"; }
-          { name = "zig-learn"; url = "https://ziglearn.org"; }
-          { name = "zig-reddit"; url = "https://old.reddit.com/r/Zig/"; }
-          { name = "zig-releases"; url = "https://ziglang.org/download/"; }
-        ]; }
-        { name = "JavaScript"; children = [
-          { name = "carbon"; url = "https://www.carbondesignsystem.com/"; }
-          { name = "carbon-components-src"; url = "https://github.com/IBM/carbon-components-svelte"; }
-          { name = "carbon-components-storybook"; url = "https://ibm.github.io/carbon-components-svelte"; }
-          { name = "carbon-icons"; url = "https://www.carbondesignsystem.com/guidelines/icons/library"; }
-          { name = "nodejs-api"; url = "https://nodejs.org/api/"; }
-          { name = "nodejs-docs"; url = "https://nodejs.org/en/docs/"; }
-          { name = "svelte"; url = "https://svelte.dev/"; }
-          { name = "svelte-docs"; url = "https://svelte.dev/docs"; }
-          { name = "svelte-examples"; url = "https://svelte.dev/examples"; }
-          { name = "svelte-spa-router"; url = "https://github.com/ItalyPaleAle/svelte-spa-router"; }
-        ]; }
-        { name = "Crystal"; children = [
-          # FIXME
-        ]; }
-        { name = "Nim"; children = [
+          { name = "go-awesome"; url = "https://github.com/avelino/awesome-go"; }
+          { name = "go-by-example"; url = "https://gobyexample.com/"; }
+          { name = "go-docs"; url = "https://golang.org/doc/"; }
+          { name = "go-lang"; url = "https://golang.org/"; }
+          { name = "go-pkgs"; url = "https://golang.org/pkg/"; }
+          { name = "go-playground"; url = "https://play.golang.org/"; }
+          { name = "go-reddit"; url = "https://www.reddit.com/r/golang"; }
+          { name = "haskell-docs"; url = "https://www.haskell.org/documentation/"; }
+          { name = "haskell-hackage"; url = "http://hackage.haskell.org/"; }
+          { name = "haskell-hoogle"; url = "https://hoogle.haskell.org/"; }
+          { name = "haskell-lang"; url = "https://www.haskell.org/"; }
+          { name = "haskell-stackage"; url = "https://www.stackage.org/"; }
           { name = "nim-curated"; url = "https://github.com/nim-lang/Nim/wiki/Curated-Packages"; }
           { name = "nim-lang"; url = "https://nim-lang.org/"; }
           { name = "nim-manual"; url = "https://nim-lang.org/docs/manual.html"; }
@@ -216,23 +190,26 @@
           { name = "nim-tutorial-p1"; url = "https://nim-lang.org/docs/tut1.html"; }
           { name = "nim-tutorial-p2"; url = "https://nim-lang.org/docs/tut2.html"; }
           { name = "nim-tutorial-p3"; url = "https://nim-lang.org/docs/tut3.html"; }
-        ]; }
-        { name = "Go"; children = [
-          # FIXME
-        ]; }
-        { name = "Python"; children = [
-          # FIXME
-        ]; }
-        { name = "Ruby"; children = [
-          # FIXME
-        ]; }
-        { name = "Ocaml"; children = [
-          # FIXME
-        ]; }
-        { name = "Haskell"; children = [
-          # FIXME
-        ]; }
-        { name = "Rust"; children = [
+          { name = "nix-manual"; url = "https://nixos.org/nix/manual/"; }
+          { name = "nixos-discourse"; url = "https://discourse.nixos.org/"; }
+          { name = "nixos-manual"; url = "https://nixos.org/nixos/manual/"; }
+          { name = "nixos-options"; url = "https://nixos.org/nixos/options.html"; }
+          { name = "nixos-reddit"; url = "https://old.reddit.com/r/nixos/"; }
+          { name = "nixos-wiki"; url = "https://nixos.wiki/"; }
+          { name = "nixpkgs-manual"; url = "https://nixos.org/nixpkgs/manual/"; }
+          { name = "nodejs-api"; url = "https://nodejs.org/api/"; }
+          { name = "nodejs-docs"; url = "https://nodejs.org/en/docs/"; }
+          { name = "ocaml-docs"; url = "https://ocaml.org/docs/"; }
+          { name = "ocaml-lang"; url = "https://ocaml.org/"; }
+          { name = "ocaml-opam"; url = "https://opam.ocaml.org/"; }
+          { name = "python-docs"; url = "https://docs.python.org/3/"; }
+          { name = "python-lang"; url = "https://www.python.org/"; }
+          { name = "python-pypi"; url = "https://pypi.org/"; }
+          { name = "ruby-awesome"; url = "https://github.com/markets/awesome-ruby"; }
+          { name = "ruby-docs"; url = "https://www.ruby-lang.org/en/documentation/"; }
+          { name = "ruby-gems"; url = "https://rubygems.org/"; }
+          { name = "ruby-lang"; url = "https://www.ruby-lang.org/en/"; }
+          { name = "ruby-reddit"; url = "https://www.reddit.com/r/ruby"; }
           { name = "rust-awesome"; url = "https://github.com/rust-unofficial/awesome-rust"; }
           { name = "rust-book"; url = "https://doc.rust-lang.org/book/"; }
           { name = "rust-by-example"; url = "https://doc.rust-lang.org/stable/rust-by-example/"; }
@@ -243,13 +220,32 @@
           { name = "rust-rustc-book"; url = "https://doc.rust-lang.org/rustc/index.html"; }
           { name = "rust-rustdoc-book"; url = "https://doc.rust-lang.org/rustdoc/index.html"; }
           { name = "rust-std"; url = "https://doc.rust-lang.org/std/index.html"; }
-        ]; }
-        { name = "Scala"; children = [
           { name = "scala-dex"; url = "https://index.scala-lang.org/"; }
           { name = "scala-docs"; url = "https://docs.scala-lang.org/"; }
           { name = "scala-lang"; url = "https://scala-lang.org/"; }
           { name = "scala-reddit"; url = "https://reddit.com/r/scala"; }
           { name = "scala-std"; url = "https://www.scala-lang.org/api/current/"; }
+          { name = "svelte"; url = "https://svelte.dev/"; }
+          { name = "svelte-carbon-docs"; url = "https://carbon-svelte.vercel.app/"; }
+          { name = "svelte-carbon-src"; url = "https://github.com/IBM/carbon-components-svelte"; }
+          { name = "svelte-docs"; url = "https://svelte.dev/docs"; }
+          { name = "svelte-examples"; url = "https://svelte.dev/examples"; }
+          { name = "svelte-spa-router"; url = "https://github.com/ItalyPaleAle/svelte-spa-router"; }
+          { name = "zfs-archwiki"; url = "https://wiki.archlinux.org/index.php/ZFS"; }
+          { name = "zfs-gentoowiki"; url = "https://wiki.gentoo.org/wiki/ZFS"; }
+          { name = "zfs-handbook"; url = "https://www.freebsd.org/doc/handbook/zfs.html"; }
+          { name = "zfs-nixoswiki"; url = "https://nixos.wiki/wiki/NixOS_on_ZFS"; }
+          { name = "zfs-oracle"; url = "https://docs.oracle.com/cd/E26505_01/html/E37384/"; }
+          { name = "zfs-reddit"; url = "https://old.reddit.com/r/zfs/"; }
+          { name = "zig-awesome"; url = "https://github.com/nrdmn/awesome-zig"; }
+          { name = "zig-docs"; url = "https://ziglang.org/documentation/0.6.0/"; }
+          { name = "zig-docs-std"; url = "https://ziglang.org/documentation/0.6.0/std/"; }
+          { name = "zig-github"; url = "https://github.com/ziglang/zig"; }
+          { name = "zig-issues"; url = "https://github.com/ziglang/zig/issues"; }
+          { name = "zig-lang"; url = "https://ziglang.org"; }
+          { name = "zig-learn"; url = "https://ziglearn.org"; }
+          { name = "zig-reddit"; url = "https://old.reddit.com/r/Zig/"; }
+          { name = "zig-releases"; url = "https://ziglang.org/download/"; }
         ]; }
       ];
 
